1be649b0633c77e60ab6c838e13d142c6db4c95e
PixelDough <alworrell19@gmail.com>
2025-03-15 23:47:40 -0500

    Fixed chaser system allowing motors to go upward

--- ForestGame/ComponentSystems/ChaserSystem.cs
+++ ForestGame/ComponentSystems/ChaserSystem.cs
@@ -10,15 +10,28 @@ public class ChaserSystem : ISystem
 {
     public void Update()
     {
-        EcsManager.world.Query(new QueryDescription().WithAll<Chaser, Transform>(), (Entity entity, ref Chaser chaser, ref Transform transform) =>
+        EcsManager.world.Query(
+            new QueryDescription()
+                .WithAll<Chaser, Transform>()
+                .WithAny<Transform, Motor>(),
+            (Entity entity, ref Chaser chaser, ref Transform transform) =>
         {
             if (!chaser.Target.IsAlive()) return;
             if (!chaser.Target.Entity.TryGet(out Transform targetTransform)) return;
+
+            Vector3 pos = transform.WorldPosition;
+            Vector3 targetPos = targetTransform.WorldPosition;
             if (entity.TryGet(out Motor motor))
             {
-                motor.MovementDirection = Vector3.Normalize(targetTransform.Position - transform.Position);
-                entity.Set(motor);
+                UpdateMotor(entity, motor, pos, targetPos);
             }
         });
+    }
+
+    private void UpdateMotor(Entity entity, Motor motor, Vector3 position, Vector3 targetPosition)
+    {
+        Vector3 direction = MathUtil.ProjectOnPlane(Vector3.Normalize(targetPosition - position), Vector3.UnitY);
+        motor.MovementDirection = direction;
+        entity.Set(motor);
     }
 }
